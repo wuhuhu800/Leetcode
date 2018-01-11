0132cc3a12f0535e676f4d03928b8281d6bacde0
阿虎_Woody <wuhuhu800@126.com>
2018-01-11 10:59:15 +0800

    Update 11 Container With Most Water.py

--- python3/11 Container With Most Water.py
+++ python3/11 Container With Most Water.py
@@ -4,3 +4,23 @@ Given n non-negative integers a1, a2, ..., an, where each represents a point at 
 Note: You may not slant the container and n is at least 2.
 
 '''
+#此题是复杂度题
+class Solution: #遍历一次
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        s_low =0
+        s_high = len(height)-1
+        s_all=[]
+        while s_low < s_high:
+            s_width = s_high-s_low
+            s_leigth = min(height[s_low],height[s_high])
+            s_area = s_width*s_leigth
+            if height[s_low] < height[s_high]:
+                s_low  +=1
+            else:
+                s_high -=1
+            s_all.append(s_area)
+        return max(s_all)
